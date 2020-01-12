831d0b91a695feafb0a077c5e1faef3e4c7601e9
Antonio Nascimento <aeduardoamaral@gmail.com>
2020-01-12 19:35:51 -0300

    Add files via upload

---
+++ Arrays.php
@@ -0,0 +1,39 @@
+<?php
+//Arrays
+
+
+$lista=array(5);
+
+
+$i=1;
+g:
+$lista[0]="Carro";
+$lista[1]="Avião";
+$lista[2]="Navio";
+$lista[3]="Moto";
+$lista[4]="Onibus";
+
+$vtor=array(
+        array("Ford","Focus"),
+        array("VW","Polo"),
+        array("Chevrolet","Prisma"),
+        array("Fiat","Toro")
+
+
+);
+
+
+
+
+echo "||=>  ".$vtor[0][0]."  -  ".$vtor[0][1]."<hr/>";
+
+echo "||=>  ".$vtor[1][0]."  -  ".$vtor[1][1]."<hr/>";
+
+echo "||=>  ".$vtor[2][0]."  -  ".$vtor[2][1]."<hr/>";
+
+echo "||=>  ".$vtor[3][0]."  -  ".$vtor[3][1]."<hr/>";
+
+echo "||=>  ".$lista[$i]."<hr/>";
+
+
+?>
